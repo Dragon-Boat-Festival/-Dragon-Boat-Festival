f0d1f73dbd04eb9bd4238aba5d6a6f92d2b3223f
huaungwj <48433992+huaungwj@users.noreply.github.com>
2021-12-20 14:45:29 +0800

    Revert "update"
    This reverts commit 57bfe24bdbc8f240343aed86b2c444c219b5ebbb.

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  publicPath: '/static/dragonBoat',
-  configureWebpack: {
-    resolve: {
-      //配置别名
-      alias: {
-        assets: '@/assets',
-        components: '@/components',
-        network: '@/network',
-        views: '@/views',
-      },
-    },
-  },
-};
+	publicPath: "./",
+	configureWebpack: {
+		resolve: {
+			//配置别名
+			alias: {
+				'assets': '@/assets',
+				'components': '@/components',
+				'network': '@/network',
+				'views': '@/views'
+			}
+		}
+	}
+}
